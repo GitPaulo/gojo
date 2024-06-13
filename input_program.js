d634c2e879f439417e7ffa165037d5da0532541f
GitPaulo <paulo.santos98@gmail.com>
2024-06-13 16:45:53 +0100

    full current capabilities example

--- input_program.js
+++ input_program.js
@@ -14,4 +14,14 @@ if (x > y) {
     const result = "x less than or equal to y";
 }
 
+// Crap while Loop (setting vars doesnt work yet)
+var i = 0;
+while (i < 5) {
+    console.log("while loop!");
+    var i = 5;
+}
+
+// Built in Functions
 console.log("Hello World!");
+console.log(Math.sqrt(16));
+console.log(Math.pow(3, 2));
