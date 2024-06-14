673afbf12acb41100c3bbec79968fff946fef7af
GitPaulo <paulo.santos98@gmail.com>
2024-06-14 12:11:49 +0100

    update input_program.js

--- input_program.js
+++ input_program.js
@@ -1,2 +1,27 @@
+// Variables & Arithmetic
 var x = 1;
-x = 4;
+var y = 1 + 1;
+var z = x + y;
+
+// Boolean Logic
+var and = true && false;
+var or = true || false;
+
+// Conditional Statements
+if (x > y) {
+    const result = "x greater than y";
+} else {
+    const result = "x less than or equal to y";
+}
+
+// While Loop
+var i = 0;
+while (i < 5) {
+    console.log("while loop!");
+    i = i + 1;
+}
+
+// Built in Functions
+console.log("Hello World!");
+console.log(Math.sqrt(16));
+console.log(Math.pow(3, 2));
